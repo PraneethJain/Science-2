ee6e09e252c96cf5f575ada2367af9fa0a88d9d2
PraneethJain <praneethjain005@gmail.com>
2024-04-16 23:10:08 +0530

    feat: bootstrap and no bootstrap outputs

--- Tutorial-2/run.sh
+++ Tutorial-2/run.sh
@@ -14,14 +14,15 @@ echo -n "enter phylip exe path: "
 read exe_path
 
 
+mkdir no_bootstrap
+cd no_bootstrap
 for item in "${items[@]}"; do
   mkdir "${item}"
   echo -n "enter ${item} input path: "
-  read path
-  cp "../${path}" "${item}/infile"
+  cp "../../${item}" "${item}/infile"
   cd "${item}"
   for a in "${A[@]}"; do
-    echo "Y" | ./"../../${exe_path}/${item}${a}"
+    echo "Y" | ./"../../../${exe_path}/${item}${a}"
     
     mkdir "${a}"
     mv outfile "${a}"
@@ -29,7 +30,7 @@ for item in "${items[@]}"; do
 
     cd "${a}"
     for plot in "${plots[@]}"; do
-      (echo ./"../../../${exe_path}" && echo "Y") | ./"../../../${exe_path}/draw${plot}"
+      (echo ./"../../../../${exe_path}" && echo "Y") | ./"../../../../${exe_path}/draw${plot}"
       mkdir "${plot}"
       mv plotfile "${plot}"
     done
@@ -37,16 +38,61 @@ for item in "${items[@]}"; do
   done
 
   for b in "${B[@]}"; do
-    echo "Y" | ./"../../${exe_path}/${item}${b}"
+    echo "Y" | ./"../../../${exe_path}/${item}${b}"
 
     mkdir "${b}"
     mv outfile "${b}/infile"
 
     cd "${b}"
-    echo "Y" | ./"../../../${exe_path}/neighbor"
+    echo "Y" | ./"../../../../${exe_path}/neighbor"
     mv outtree intree
     for plot in "${plots[@]}"; do
-      (echo ./"../../../${exe_path}" && echo "Y") | ./"../../../${exe_path}/draw${plot}"
+      (echo ./"../../../../${exe_path}" && echo "Y") | ./"../../../../${exe_path}/draw${plot}"
+      mkdir "${plot}"
+      mv plotfile "${plot}"
+    done
+    cd ..
+  done
+  cd ..
+done
+cd ..
+
+mkdir bootstrap
+cd bootstrap
+for item in "${items[@]}"; do
+  mkdir "${item}"
+  echo -n "enter ${item} input path: "
+  cp "../../${item}" "${item}/infile"
+  cd "${item}"
+  ./"../../../${exe_path}/seqboot" <<< "Y"
+  mv outfile infile
+  for a in "${A[@]}"; do
+    echo "Y" | ./"../../../${exe_path}/${item}${a}"
+    
+    mkdir "${a}"
+    mv outfile "${a}"
+    mv outtree "${a}/intree"
+
+    cd "${a}"
+    for plot in "${plots[@]}"; do
+      (echo ./"../../../../${exe_path}" && echo "Y") | ./"../../../../${exe_path}/draw${plot}"
+      mkdir "${plot}"
+      mv plotfile "${plot}"
+    done
+    cd ..
+  done
+
+  for b in "${B[@]}"; do
+    echo "Y" | ./"../../../${exe_path}/${item}${b}"
+
+    mkdir "${b}"
+    mv outfile "${b}/infile"
+
+    cd "${b}"
+    echo "Y" | ./"../../../../${exe_path}/neighbor"
+    mv outtree intree
+    for plot in "${plots[@]}"; do
+      (echo ./"../../../../${exe_path}" && echo "Y") | ./"../../../../${exe_path}/draw${plot}"
       mkdir "${plot}"
       mv plotfile "${plot}"
     done
